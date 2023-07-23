bbb7135a9481a4ffb1096c36b0a79e755d335e14
sv2 <sv2@slana.tech>
2023-07-23 03:02:26 -0700

    google analytics

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -16,6 +16,16 @@ module.exports = {
       level: [2, 6],
     },
   },
+  head: [
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-9YYSL8KCZV',
+      },
+    ],
+    ['script', {}, "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-9YYSL8KCZV');"],
+  ],
   themeConfig: {
     repo: 'https://github.com/',
     pages: fetchDocs(),
